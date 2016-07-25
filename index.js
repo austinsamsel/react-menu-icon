a891ea2ca12cf9b77a8cc9d49ce0bed93f410e26
austin samsel <austinsamsel@gmail.com>
2016-07-24 21:44:53 -0400

    dont need styleroot anymore since no keyframes

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MenuIcon from './MenuIcon';
-import {StyleRoot} from 'radium';
 
 ReactDOM.render(
-    <StyleRoot>
-      <MenuIcon /> 
-    </StyleRoot>,
+    <MenuIcon />,
     document.getElementById('root')  
   );
